bf3ba846b3a118c428fae70595000f113738eaa5
Luis Solorzano <Solorzanoluis@hotmail.com>
2021-04-07 10:56:59 -0400

    map function in otu ids values

--- static/js/app.js
+++ static/js/app.js
@@ -78,9 +78,9 @@ Object.entries(params1[0]).forEach(([key, value]) => {
 // var text = params2.map(row => row[0]) // .otu_labels;
 // console.log(text);
 
-var text = params2[0].otu_labels.slice(0,10);
-var x_axis = params2[0].sample_values.slice(0,10);
-var y_axis = params2[0].otu_ids.slice(0,10).map(ID => `OTU  ${ID}`);
+var text = params2[0].otu_labels.slice(0,10).reverse();
+var x_axis = params2[0].sample_values.slice(0,10).reverse();
+var y_axis = params2[0].otu_ids.slice(0,10).map(ID => `OTU → ${ID}`).reverse();
 console.log(x_axis)
 console.log(y_axis)
 console.log(text)
@@ -101,7 +101,7 @@ console.log(trace1.x)
 var data = [trace1];
 
 var layout = {
-  title: 'top 10 OTUs found',
+  title: 'Top 10 OTUs found',
   margin: { t: 30, l: 150 }
   // font:{
   //   family: 'Raleway, sans-serif'
