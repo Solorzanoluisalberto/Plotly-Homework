a6b54aa3841771b340f7e54f7e10376d4c36aebd
Luis Solorzano <Solorzanoluis@hotmail.com>
2021-04-07 22:01:53 -0400

    2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.

--- static/js/app.js
+++ static/js/app.js
@@ -10,35 +10,34 @@ function unpack(rows, index) {
 
 function namesTest(Test){
 d3.json("../../data/samples.json").then(function(data) {
-let new_plot = true;
-if (Test.length == 0){
-  let Test_1 = data.names[0];
-  console.log(Test_1)
-  var Selection_Samples = data["samples"].filter(function(sample) {
-  return sample.id == Test_1;
-  });
-  var Selection_metadata = data["metadata"].filter(function(meta) {
-    return meta.id == Test_1;
-  });
-  console.log(Selection_Samples);
-  console.log(Selection_metadata);
-  // Print_Test(Selection_metadata, Selection_Samples, new_plot)
-} else{
-    new_plot = false;
-    console.log("new dato from funtion button")
-    let New_Test = Test
-    console.log(New_Test)
+  let new_plot = true;
+  if (Test.length == 0){
+    let Test_1 = data.names[0];
+    console.log(Test_1)
     var Selection_Samples = data["samples"].filter(function(sample) {
-      return sample.id == New_Test;
-      });
+    return sample.id == Test_1;
+    });
     var Selection_metadata = data["metadata"].filter(function(meta) {
-      return meta.id == New_Test;
+      return meta.id == Test_1;
     });
     console.log(Selection_Samples);
     console.log(Selection_metadata);
     // Print_Test(Selection_metadata, Selection_Samples, new_plot)
-}
-Print_Test(Selection_metadata, Selection_Samples, new_plot)
+  } else{
+      new_plot = false;
+      console.log("new dato from funtion button")
+      let New_Test = Test
+      console.log(New_Test)
+      var Selection_Samples = data["samples"].filter(function(sample) {
+        return sample.id == New_Test;
+        });
+      var Selection_metadata = data["metadata"].filter(function(meta) {
+        return meta.id == New_Test;
+      });
+      console.log(Selection_Samples);
+      console.log(Selection_metadata);
+      // Print_Test(Selection_metadata, Selection_Samples, new_plot)
+  }
 
   //  var newData = data.
   let names = data.names;
@@ -54,87 +53,113 @@ Print_Test(Selection_metadata, Selection_Samples, new_plot)
       .append('option')
       .text(function (d) { return d; });
       // console.log(names);
+  Print_Test(Selection_metadata, Selection_Samples, new_plot)
    });
-  }
+}
 
-var init = ""
-namesTest(init);
-
-function Print_Test(params1, params2, plot) {
-  console.log(params1)
-  console.log(params2[0].sample_values)
-  console.log("new Plot? " + plot)
+function Print_Test(metadata, samples, new_plot) {
+  console.log(metadata)
+  console.log(samples[0].sample_values)
+  console.log("new Plot? " + new_plot)
   
 var sample_metadata1 = d3.select("#sample-metadata")
 sample_metadata1.html("")
-Object.entries(params1[0]).forEach(([key, value]) => {
+Object.entries(metadata[0]).forEach(([key, value]) => {
   var Paragraph = sample_metadata1.append("p");
   Paragraph.text(`${key} : ${value}`)
   .attr("class","text-success")
-
 });
+
 // var text = unpack(params2[0],0)
 // console.log(params2)
 // var text = params2.map(row => row[0]) // .otu_labels;
 // console.log(text);
 
-var text = params2[0].otu_labels.slice(0,10).reverse();
-var x_axis = params2[0].sample_values.slice(0,10).reverse();
-var y_axis = params2[0].otu_ids.slice(0,10).map(ID => `OTU → ${ID}`).reverse();
-console.log(x_axis)
-console.log(y_axis)
-console.log(text)
+var sample_values = samples[0].sample_values.slice(0,10).reverse();
+var otu_ids = samples[0].otu_ids.slice(0,10).map(ID => `OTU → ${ID}`).reverse();
+var otu_labels = samples[0].otu_labels.slice(0,10).reverse();
 
+
+console.log("sample_values " + sample_values);
+console.log("otu_ids " + otu_ids);
+console.log("otu_labels " + otu_labels);
+
+// Create a horizontal bar chart
 //  ========================================================
-var trace1 = {
-  x: x_axis,
-  y: y_axis,
+
+var trace1_bar = {
+  x: sample_values,
+  y: otu_ids,
   type: 'bar',
-  text: text,
-  orientation: 'h'
-  // marker: {
-  //   color: 'rgb(142,124,195)'
-  // }
-};
-console.log(trace1.x)
-
-var data = [trace1];
-
-var layout = {
-  title: 'Top 10 OTUs found',
-  margin: { t: 30, l: 150 }
-  // font:{
-  //   family: 'Raleway, sans-serif'
-  // },
-  // showlegend: false,
-  // xaxis: {
-  //   tickangle: -15
-  // },
-  // yaxis: {
-  //   zeroline: false,
-  //   gridwidth: 2
-  // },
-  // // bargap :0.05
+  text: otu_labels,
+  orientation: 'h',
+  marker: {
+    color: 'rgb(142,124,195)'
+  }
 };
 
-if (plot) {
-  Plotly.newPlot('bar', data, layout);
+var data_bar = [trace1_bar];
+
+var layout_bar = {
+  title: 'Top 10 OTUs ',
+  orientation: 'h',
+  font:{
+    family: 'Raleway, sans-serif'
+  },
+  showlegend: false,
+  xaxis: {
+    tickangle: -45
+  },
+  yaxis: {
+    zeroline: false,
+    gridwidth: 2
+  },
+  bargap :0.05
+};
+// ============================================================================
+// Create a bubble chart that displays each sample.
+let otu_ids_bubble = samples[0].otu_ids
+let sample_values_bubble = samples[0].sample_values
+let otu_labels_bubble = samples[0].otu_labels
+
+var trace1_bubble = {
+  x: otu_ids_bubble,
+  y: sample_values_bubble,
+  mode: 'markers',
+  marker: {
+    color: otu_ids_bubble,
+    opacity: [1, 0.8, 0.6, 0.4],
+    size: sample_values_bubble
+  }
+};
+
+var data_bubble = [trace1_bubble];
+
+var layout_bubble = {
+  title: 'Marker Size and Color',
+  showlegend: false
+  // height: 600,
+  // width: 600
+};
+
+// ========================================================================
+if (new_plot) {
+  Plotly.newPlot('bar', data_bar, layout_bar);
+  Plotly.newPlot('bubble', data_bubble, layout_bubble);
 } else {
-  // Note the extra brackets around 'x' and 'y'
-  // Note the extra brackets around 'x' and 'y'
-  Plotly.restyle("bar", "x", [trace1.x]);
-  Plotly.restyle("bar", "y", [trace1.y]);
+  
+  Plotly.restyle("bar", "x", [trace1_bar.x]);
+  Plotly.restyle("bar", "y", [trace1_bar.y]);
+  Plotly.newPlot('bubble', data_bubble, layout_bubble);
 }
-// Plotly.newPlot('bar', data, layout);
-// // // Plotly.newPlot('sample-metadata', data);
 
-}
+};
 
 // nombres("940");
 function optionChanged(Test) {
   console.log(Test)
-
   namesTest(Test)
 }
 
-
+var init = ""
+namesTest(init);
