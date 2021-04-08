d35b564c859f88680f3572186d0bf581642f5297
Luis Solorzano <Solorzanoluis@hotmail.com>
2021-04-07 23:30:36 -0400

    bubble chart that displays each sample

--- static/js/appBonus.js
+++ static/js/appBonus.js
@@ -145,58 +145,6 @@ if (new_plot) {
   Plotly.restyle("bar", "y", [trace1_bar.y]);
   Plotly.newPlot('bubble', data_bubble, layout_bubble);
 }
-// =========================================================
-// Advanced Challenge Assignment (Optional)
-// var value_gauge = Math.max(samples[0].sample_values);
-// console.log(samples[0].sample_values);
-// console.log(value_gauge);
-// Object.entries(metadata[0]).forEach(([key, value]) => {
-var wfreq = metadata[0].wfreq;
-console.log("Frecuencia: " + wfreq);
-
-var data = [
-  {
-    type: "indicator",
-    mode: "gauge+number+delta",
-    value: wfreq,
-    title: { text: "Speed", font: { size: 24 } },
-    delta: { reference: 9, increasing: { color: "RebeccaPurple" } },
-    gauge: {
-      axis: { range: [0, 9], tickwidth: 1, tickcolor: "darkblue" },
-      bar: { color: "darkblue" },
-      bgcolor: "white",
-      borderwidth: 2,
-      bordercolor: "gray",
-      steps: [
-        { range: [0, 1], color: "cyan" },
-        { range: [1, 2], color: "royalblue" },
-        { range: [2, 3], color: "cyan" },
-        { range: [3, 4], color: "royalblue" },
-        { range: [4, 5], color: "cyan" },
-        { range: [5, 6], color: "royalblue" },
-        { range: [6, 7], color: "cyan" },
-        { range: [7, 8], color: "royalblue" },
-        { range: [8, 9], color: "cyan" }
-      ],
-      threshold: {
-        line: { color: "red", width: 4 },
-        thickness: 0.75,
-        value: wfreq
-      }
-    }
-  }
-];
-
-var layout = {
-  // width: 500,
-  // height: 400,
-  margin: { t: 25, r: 25, l: 25, b: 25 },
-  paper_bgcolor: "lavender",
-  font: { color: "darkblue", family: "Arial" }
-};
-
-Plotly.newPlot('gauge', data, layout);
-
 
 };
 
